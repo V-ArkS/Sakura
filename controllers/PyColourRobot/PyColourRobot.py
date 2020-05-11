bd755eebacf1804fde8c6b04a5ee70a775ae6333
Buoy919 <61744014+Buoy919@users.noreply.github.com>
2020-05-11 09:47:02 +0800

    colored box which can generate random colors
    colored box which can generate random colors

--- controllers/PyColourRobot/PyColourRobot.py
+++ controllers/PyColourRobot/PyColourRobot.py
@@ -1,7 +1,14 @@
-from controller import Robot
+from controller import Robot 
+import random
 robot = Robot()
 motor = robot.getMotor('motor1')
-TIME_STEP = 64
+TIME_STEP = 64 
+number = random.randint(1,3)
 while robot.step(TIME_STEP) != -1:
     motor.setVelocity(0.5)
-    motor.setPosition(-1.4)
+    if number == 1:
+        motor.setPosition(-1.4)
+    elif number == 2:
+        motor.setPosition(-0.7)
+    elif number == 3:
+        motor.setPosition(0) 
